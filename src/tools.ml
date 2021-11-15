6787d46be9b10628792723917b810d020778f04c
scanim.valentin <valentin.guerbette@hotmail.fr>
2021-11-15 16:20:53 +0100

    graphe

--- src/tools.ml
+++ src/tools.ml
@@ -2,6 +2,8 @@
 open Graph
 
 (* assert false is of type ∀α.α, so the type-checker is happy. *)
-let clone_nodes (gr:'a graph) = assert false
+let clone_nodes (gr : a' graph) = n_fold gr new_node empty_graph 
+
 let gmap gr f = assert false
-...
+
+
