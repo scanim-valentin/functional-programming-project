f46fb1f20a69ea1628ff1032e165a22cbf80fb0b
scanim.valentin <valentin.guerbette@hotmail.fr>
2021-11-15 16:50:42 +0100

    on en est là

--- src/tools.ml
+++ src/tools.ml
@@ -4,6 +4,10 @@ open Graph
 (* assert false is of type ∀α.α, so the type-checker is happy. *)
 let clone_nodes gr = n_fold gr new_node empty_graph 
 
-let gmap gr f = assert false
+(*On en est là lol*)
+let gmap gr f = 
+  let rec gmap_aux gr acc f =  
+  in 
+  e_fold gr gmap_aux gr (clone_nodes gr) 
 
 let add_arc gr n1 n2 p = assert false
