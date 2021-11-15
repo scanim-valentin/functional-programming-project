5568f330b985dae6025e7a92f2f3efb5609b3933
“Jean6-971” <jean.vaninghelandt@outlook.fr>
2021-11-15 01:51:11 +0100

    Added Tools

---
+++ src/tools.ml
@@ -0,0 +1,7 @@
+(* Yes, we have to repeat open Graph. *)
+open Graph
+
+(* assert false is of type ∀α.α, so the type-checker is happy. *)
+let clone_nodes (gr:'a graph) = assert false
+let gmap gr f = assert false
+...
