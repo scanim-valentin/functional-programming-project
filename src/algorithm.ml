a42f29a64176bc0205c3f2ee56322038c59901a9
scanim.valentin <valentin.guerbette@hotmail.fr>
2021-11-22 16:06:18 +0100

    gr_gap

--- src/algorithm.ml
+++ src/algorithm.ml
@@ -1,4 +1,11 @@
 open Graph
 open Tools
 
-let ford_fulkerson gr id1 id2 = 
+let ford_fulkerson gr_int id1 id2 = 
+  (*Initializing the flow*)
+  let init_ff gr = gmap gr_int ( fun lbl->(0,lbl) ) in (*gr : flow graph*)
+  
+  (*Execution*)
+  let gr_gap = e_fold (clone_nodes gr_int) ( fun gr n1 n2 (lbl1,lbl2) -> ( add_arc gr n1 n2 lbl1 ) ; ( add_arc gr n2 n1 lbl2 ) ) in 
+  (*gr_gap : int graph*)
+  
