b79d491ccfc513f7f5468758c5aa4e9a8c974dad
scanim.valentin <valentin.guerbette@hotmail.fr>
2021-11-23 11:56:06 +0100

    add_double_arc

--- src/algorithm.ml
+++ src/algorithm.ml
@@ -3,9 +3,9 @@ open Tools
 
 (*Initializing the flow*)
 let init_ff gr_int = gmap gr_int ( fun lbl->(0,lbl) ) (*gr : flow graph*)
-let add_double_arc gr n1 n2 (lbl1,lbl2) = 
-  add_arc gr n1 n2 lbl1 ;
-  add_arc gr n2 n1 lbl2
+let add_double_arc gr_int n1 n2 (lbl1,lbl2) = 
+  let gr_r = add_arc gr_int n1 n2 lbl1 in
+  add_arc gr_int n2 n1 lbl2 
 let gr_gap gr_flow = e_fold gr_flow add_double_arc (clone_nodes gr_flow)
   (*gr_gap : int graph*)  
 (*
--- src/algorithm.mli
+++ src/algorithm.mli
@@ -7,4 +7,4 @@ val init_ff: int graph -> (int*int) graph
 
 val gr_gap : (int*int) graph -> int graph
 
-val add_double_arc : (int*int) graph -> id -> id -> int -> int graph
+val add_double_arc : int graph -> id -> id -> (int*int) -> int graph
