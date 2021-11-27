f9ceec26045783b3b7d03b78e851693d3e9373ee
scanim.valentin <valentin.guerbette@hotmail.fr>
2021-11-27 17:23:13 +0100

    dijkstra

--- src/dijkstra.ml
+++ src/dijkstra.ml
@@ -9,12 +9,16 @@ let labelize gr_int =
     let add_lbl arr id = Array.append arr (Array.make 1 (Inf, id, False) )  in
     
     (*Init lbl_array to Empty Array*)
-    let label Array lbl_array = Array.make 0 (Inf,0,False) in
+    let lbl_array = Array.make 0 (Inf,0,False) in
     
     (*Folding on all nodes to make lbl_array*)
     n_fold gr_int add_lbl lbl_array
     
-
+let dijkstra gr_int = 
+    
+    let lbl_array = labelize gr_int in
+    
+    
     
     
     
--- src/dijkstra.mli
+++ src/dijkstra.mli
@@ -2,10 +2,12 @@ open Graph
 open Tools
 
 type cost = Inf | Finite of int
+
 type label = (int * id * bool)
 
 (*Generates a list of label from a graph of int, intialised to (Inf, id, False) *)
 val labelize : int graph -> label Array
 
+(*Will find a shortest path (i.e. a list of node) in a graph. Returns an empty list if no path exists between id1 and id2*)
 val dijkstra : int graph -> id -> id -> id list
 
