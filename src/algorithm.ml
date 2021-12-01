53be9e6583f560f0d582b37f8b286a06d4b80768
scanim.valentin <valentin.guerbette@hotmail.fr>
2021-12-01 19:51:40 +0100

    it compiles

--- src/algorithm.ml
+++ src/algorithm.ml
@@ -20,23 +20,24 @@ let rec first_non_marked out_arc_lst marked_nodes_lst =
         
 (*Returns flow variation for a path*)
     (*flow_variation: (id*int) list -> int -> int *)
-    let rec flow_variation path min_acc = match path with 
+let rec flow_variation path min_acc = match path with 
      |[] -> 0 
-     |(_,val)::tl -> (*Getting the minimal gap*)
-        if( val < min_acc )then 
-            flow_variation tl val 
+     |(_,value)::tl -> (*Getting the minimal gap*)
+        if( value < min_acc )then 
+            flow_variation tl value 
         else 
             flow_variation tl min_acc 
 
 (*cursed*)
-    let rec update_graph path gr_int mn = match path with
+let rec update_graph path gr_int mn = match path with
         |[] -> gr_int
-        |(id1,val1)::((id2,val2)::tl) -> match ((find_arc gr_int id1 id2),(find_arc gr_int id1 id2)) with
-            |(None,None) -> update_graph ((id2,val2)::tl) gr_int mn 
-            |(None,Some x) ->update_graph ((id2,val2)::tl) (new_arc gr_int id2 id1 x-mn) mn ; 
-            |(Some x,None) -> update_graph ((id2,val2)::tl) (new_arc gr_int id1 id2 x+mn) mn  ;
-            |(Some x1,Some x2) -> update_graph ((id2,val2)::tl) (new_arc (new_arc gr_int id2 id1 x2-mn) id1 id2 x1+mn) mn  ;
-        
+        |(id1,val1)::((id2,val2)::tl) -> (*Updating the out and in arc values along the given path*)
+            match ((find_arc gr_int id1 id2),(find_arc gr_int id2 id1)) with
+                |(None,None) -> update_graph ((id2,val2)::tl) gr_int mn 
+                |(None,Some x) ->update_graph ((id2,val2)::tl) (new_arc gr_int id2 id1 (x-mn)) mn ; 
+                |(Some x,None) -> update_graph ((id2,val2)::tl) (new_arc gr_int id1 id2 (x+mn)) mn  ;
+                |(Some x1,Some x2) -> update_graph ((id2,val2)::tl) (new_arc (new_arc gr_int id2 id1 (x2-mn)) id1 id2 (x1+mn)) mn  
+
 (*Finds a path in a graph between id1 and id2 based on depth search*)
 let rec find_path id1 id2 acc_path marked_nodes gr_gap = match acc_path with
         |[] -> (*beginning depth path search from id1 to id2*)
@@ -48,60 +49,20 @@ let rec find_path id1 id2 acc_path marked_nodes gr_gap = match acc_path with
                     ( match (first_non_marked out_arcs_list marked_nodes ) with
                         |None -> (*All next nodes have been marked (i.e. all path have been explored)*)
                             acc_path (*Algorithm ends here*)
-                        |Some (id_next,val) -> (*Iterating on the next node*)
-                            find_path id_next id2 [(id_next,val)] (id_next::marked_nodes) gr_gap))
+                        |Some (id_next,value) -> (*Iterating on the next node*)
+                            find_path id_next id2 [(id_next,value)] (id_next::marked_nodes) gr_gap))
                             
-        |(id_next,val)::tl -> (*is path complete?*) 
+        |(id_next,value)::tl -> (*is path complete?*) 
             if (id_next == id2) then (*path is complete*) 
                 acc_path
             else (*destination hasn't been reached yet*)
-                ( match (out_arcs gr_flow h) with
-                |[]                -> (*h is isolated*)
-                    gr_gap
-                |out_arcs_list -> (*iterating on the first node that is not 
-                                in the marked_nodes list and marking the next node*)
-                     match (first_non_marked out_arcs_list marked_nodes ) with
-                        |None -> (*All next nodes have been marked (i.e. all path have been explored)*)
-                            gr_flow (*Algorithm ends here*)
-                        |Some (id_next,(f,c)) -> (*Iterating on the next node*)
-                            find_path id_next id2 [id_next] (id_next::marked_nodes) )
-
-(*
-let ford_fulkerson gr id1 id2 = 
-    (*INIT*)
-    let init_ff gr_int = gmap gr_int ( fun lbl->(0,lbl) )
-    let gr_gap = gap_from_flow (init_ff gr) in (*Initialized gap graph*)
-    
-    (*
-    (*iter : id*(int*int) list -> id list -> (int*int) graph*)
-    let rec iter acc_path marked_nodes gr_gap = match acc_path with
-        |[] -> (*beginning depth path search from id1 to id2*)
-           ( match (out_arcs gr_gap id1) with
+                ( match (out_arcs gr_gap id1) with
                 |[]                -> (*id1 is isolated*)
-                    gr_gap
+                    acc_path
                 |out_arcs_list -> (*iterating on the first node that is not 
                                 in the marked_nodes list and marking the next node*)
                     ( match (first_non_marked out_arcs_list marked_nodes ) with
                         |None -> (*All next nodes have been marked (i.e. all path have been explored)*)
-                            gr_gap (*Algorithm ends here*)
-                        |Some (id_next,val) -> (*Iterating on the next node*)
-                            iter [(id_next,val)] (id_next::marked_nodes) gr_gap))
-                            
-        |(id_next,val)::tl -> (*is path complete?*) 
-            if (id_next == id2) then (*path is complete*) 
-                update_graph path gr_gap (flow_variation acc_path max_int) 
-            else (*destination hasn't been reached yet*)
-                ( match (out_arcs gr_flow h) with
-                |[]                -> (*h is isolated*)
-                    gr_gap
-                |out_arcs_list -> (*iterating on the first node that is not 
-                                in the marked_nodes list and marking the next node*)
-                     match (first_non_marked out_arcs_list marked_nodes ) with
-                        |None -> (*All next nodes have been marked (i.e. all path have been explored)*)
-                            gr_flow (*Algorithm ends here*)
-                        |Some (id_next,(f,c)) -> (*Iterating on the next node*)
-                            iter [id_next] (id_next::marked_nodes) )
-        in
-        *)
-        iter [] []
-    *)
+                            acc_path (*Algorithm ends here*)
+                        |Some (id_next,value) -> (*Iterating on the next node*)
+                            find_path id_next id2 [(id_next,value)] (id_next::marked_nodes) gr_gap))
--- src/algorithm.mli
+++ src/algorithm.mli
@@ -3,7 +3,7 @@ open Tools
 
 (*type cost = Inf | Finite of int*)
 
-val ford_fulkerson: (*int) graph -> id -> id -> (int*int) graph
+(*val ford_fulkerson: (int) graph -> id -> id -> (int*int) graph*)
 
 val init_ff: int graph -> (int*int) graph
 
@@ -11,3 +11,4 @@ val gap_from_flow : (int*int) graph -> int graph
 
 val add_double_arc : int graph -> id -> id -> (int*int) -> int graph
 
+val find_path : id -> id -> (id*int) list -> id list -> int graph -> (id*int) list 
