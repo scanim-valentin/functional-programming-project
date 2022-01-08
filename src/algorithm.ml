2836f7abf9bdf9b8240995d5408c4bb77a0be54d
“Jean6-971” <jean.vaninghelandt@outlook.fr>
2022-01-08 21:13:11 +0100

    Update algorithm.ml

--- src/algorithm.ml
+++ src/algorithm.ml
@@ -6,6 +6,7 @@ open Printf
 (*Returns the first out arc to a non marked AND strictly positive node or none if all have been marked *)
 (*first_non_marked: (id * int) list -> id list -> (id * int) option *)
 let rec first_non_marked out_arc_lst marked_nodes_lst =
+
   let cond_arc (idA,value) = if (value = 0) then
       false
     else 
@@ -18,6 +19,7 @@ let rec first_non_marked out_arc_lst marked_nodes_lst =
 (*Returns minimal flow for a path *)
 (*flow_variation: (id * int) list -> int -> int *)
 let flow_variation pth = 
+
   let folding acc (_,value) = 
     (match acc with  (*Here None is a substitute to infinity *)
      |None -> Some value (*Fist call case *)
@@ -32,23 +34,30 @@ let flow_variation pth =
 (*Returns updated graph with new values on arcs *)
 (*update_graph: id -> (id * 'a) list -> int graph -> int -> int graph *)
 let update_graph source pth graph min = 
+
   let rec operation src path gr_int mn =
     match path with
     |[] -> gr_int
     |[(id1,val1)] -> (*A single element in the path means theres a direct path between source and sink *)
+
       let new_gr_int = add_arc gr_int src id1 (-mn) in 
       add_arc new_gr_int id1 src mn
     |(id1,val1)::((id2,val2)::tl) -> (*Updating the out and in arc values along the given path *)
+
       let new_gr_int = add_arc gr_int id2 id1 (-mn) in 
+
       let new_gr_int = add_arc new_gr_int id1 id2 mn in
       operation src ((id2,val2)::tl) new_gr_int mn in
+
   (*Removes non positive arcs to keep a valid graph *)
   let remove_invalid_arc gr = 
+
     let only_postive gr id1 id2 value = if(value > 0) then
         new_arc gr id1 id2 value 
       else
         gr in 
     e_fold gr only_postive (clone_nodes gr) in 
+
   let updated_graph = operation source pth graph min in
   remove_invalid_arc updated_graph
 
@@ -57,7 +66,9 @@ let update_graph source pth graph min =
 (*find_path: id -> id -> (int) graph -> (id * int) list *)
 let find_path source sink graph =
   (*acc_path : the path to complete ; marked_nodes : list of marked nodes to avoid *)
+
   let rec step acc_path marked_nodes current src sk graph =
+
     let () = printf "find_path - Current node : %d | Source : %d | Sink : %d \n%!" current src sk in
     (*Checking if destination has been reached*)
     if(current = sk)then 
@@ -66,22 +77,27 @@ let find_path source sink graph =
       (*Finding the next node that hasn't been marked *)
       match(first_non_marked (out_arcs graph current) marked_nodes)with
       |None -> (*current is isolated (i.e. we need to go back) *)
+
         let () = printf "find_path -   All nodes are marked\n%!" in
         (match(acc_path)with
          |[] -> (*path is empty which means there is no available path (current = source) *)
+
            let () = printf "find_path -      No available path (current = source)\n%!" in
            []
          |_::prev_tail -> (*path is not empty which means we can go back (i.e. pop the path's head) *)
            (match(prev_tail)with
             |[] -> (*Back at the source node *)
+
               let () = printf "find_path -      Going back into source node %d\n%!" src in
               step [] (current::marked_nodes) src src sk graph
             |(previous,w)::tl -> (*Marking this node and stepping back into the previous node *)
+
               let () = printf "find_path -      Going back into : %d\n%!" previous in
               step ((previous,w)::tl) (current::marked_nodes) previous src sk graph
            )
         )
       |Some (next,w) -> (*stepping into the next non marked node *)
+
         let () = printf "find_path -    Stepping into : %d\n%!" next in
         step ((next,w)::acc_path) (current::marked_nodes) next src sk graph in
   step [] [] source source sink graph
@@ -90,23 +106,35 @@ let find_path source sink graph =
 (*Implementing the Ford-Fulkerson algorithm *)
 (*ford_fulkerson: (int) graph -> id -> id -> (int) graph *)
 let ford_fulkerson grph id1 id2 =
+
   let rec iter src sk graph i max_flow =
+
     let path = find_path src sk graph in
+
     (*Prints the path in the terminal *)
     let mapped = List.map (fun (idN, value) -> sprintf "(id %d, value %d)" idN value) path in
+
     let path_string = String.concat "<-" mapped in
+
     let () = printf "Path : %s\n%!" path_string in
     match path with 
     |[] -> (*find_path failing to find a path means the algorithm has reached its end *)
+
       let () = printf "No path\n%!" in 
+
       let () = printf "The algorithm terminated with a maximum flow value of: %d\n%!" max_flow in 
       graph
     |_::_ -> (*A path exists: updating the graph based on the minimal flow *)
+
       let flow_min = flow_variation path in
+
       let () = printf "Flow min : %d\n%!" flow_min in 
+
       (*Updating the flows along the path *)
       let updated_graph = update_graph src path graph flow_min in
+
       let string_graph = gmap updated_graph string_of_int in
+
       let () = export ("./iterations/iter"^(string_of_int i)^".dot") string_graph in
       if(flow_min = 0)then (*Impossible? *)
         graph
