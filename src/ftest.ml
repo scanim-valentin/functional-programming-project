efee75a1fdc4c2ab795a73eb856227883b638d8d
“Jean6-971” <jean.vaninghelandt@outlook.fr>
2022-01-08 20:53:42 +0100

    Update ftest.ml

--- src/ftest.ml
+++ src/ftest.ml
@@ -2,8 +2,9 @@ open Gfile
 open Tools
 open Algorithm
 open Printf
-let () =
 
+
+let () =
   (* Check the number of command-line arguments *)
   if Array.length Sys.argv <> 5 then
     begin
@@ -15,26 +16,19 @@ let () =
          "    ðŸŸ„  outfile : output file in which the result should be written.\n\n") ;
       exit 0
     end ;
-
-
   (* Arguments are : infile(1) source-id(2) sink-id(3) outfile(4) *)
 
   let infile = Sys.argv.(1)
   and outfile = Sys.argv.(4)
-
   (* These command-line arguments are not used for the moment. *)
   and source = int_of_string Sys.argv.(2)
   and sink = int_of_string Sys.argv.(3)
   in
-
   (* Open file *)
   (*
   let graph = from_file infile in
-
   let graph = gmap graph int_of_string in
-
   (* let graph = init_ff graph in *)
-
   let graph = init_ff graph in 
   let graph = gap_from_flow graph in
   let path = find_path source sink graph in
@@ -46,20 +40,20 @@ let () =
   (*let graph = gmap graph string_of_int in
     (* Rewrite the graph that has been read. *)
     let () = write_file outfile graph in
-
     let () = export (outfile^".dot") graph in
-
     ()*)
 
-  (* Open file *)
+  (*Open file *)
   let graph = from_file infile in
 
   let graph = gmap graph int_of_string in
 
+  (*Apply ford_fulkerson on graph from source to sink *)
   let graph = ford_fulkerson graph source sink in
 
   let graph = gmap graph string_of_int in
-  (* Rewrite the graph that has been read. *)
+
+  (*Rewrite the graph that has been read. *)
   let () = write_file outfile graph in
 
   let () = export (outfile^".dot") graph in
